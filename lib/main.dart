5b233cfb08f7e41b08a09a9d87dc22fa36ba68b0
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-05 16:43:51 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,7 @@
 import 'package:flutter/material.dart';
-import 'ciantis/ui/ciantis_shell.dart';
-import 'ciantis/universal/developer_logger.dart';
-import 'ciantis/universal/universal_tick_scheduler.dart';
-import 'ciantis/ui/developer_hotkey_layer.dart';
+import 'ciantis/ciantis_app.dart';
 
 void main() {
-  DeveloperLogger.log("App startup: main() invoked");
-
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Start the universal tick scheduler
-  UniversalTickScheduler.instance.start(
-    interval: const Duration(seconds: 10),
-  );
-
-  DeveloperLogger.log("Launching CiantisApp");
-
   runApp(const CiantisApp());
-}
-
-class CiantisApp extends StatelessWidget {
-  const CiantisApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    DeveloperLogger.log("CiantisApp build triggered");
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Ciantis",
-      theme: ThemeData.dark(),
-      home: const DeveloperHotkeyLayer(
-        child: CiantisShell(),
-      ),
-    );
-  }
 }
