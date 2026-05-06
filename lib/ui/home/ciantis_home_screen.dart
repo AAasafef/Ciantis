4997489c97791cb71d0618db1f6ceac48d3c247d
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-06 08:20:13 -0400

    Update ciantis_home_screen.dart

--- lib/ui/home/ciantis_home_screen.dart
+++ lib/ui/home/ciantis_home_screen.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class CiantisHomeScreen extends StatelessWidget {
+  const CiantisHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7F5F2),
+      body: Center(
+        child: Text(
+          "Ciantis Home Screen",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.w600,
+            color: Colors.black.withOpacity(0.85),
+          ),
+        ),
+      ),
+    );
+  }
+}
