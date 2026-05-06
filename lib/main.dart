bc7c753ba2370c2980d6ca20b43d9e955a863de9
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-06 08:17:56 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'ui/home/ciantis_home_screen.dart';
 
-class CiantisHomeScreen extends StatelessWidget {
-  const CiantisHomeScreen({super.key});
+void main() {
+  runApp(const CiantisApp());
+}
+
+class CiantisApp extends StatelessWidget {
+  const CiantisApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF7F5F2),
-      body: Center(
-        child: Text(
-          "Ciantis Home Screen",
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w600,
-            color: Colors.black.withOpacity(0.85),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const CiantisHomeScreen(),
     );
   }
 }
